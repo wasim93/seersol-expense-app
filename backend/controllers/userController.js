51afce4556d4c453c583e983cc48a860bfbc1fbe
Wasim Akram <muhammad.wasim93@gmail.com>
2020-12-01 17:52:56 +0500

    User Authentication Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,27 @@
 import expressAsyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
 
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
+const authUser = expressAsyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: null,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
+
 // @desc    Get all users
 // @route   GET /api/users
 // @access  Private/Admin
@@ -9,4 +30,4 @@ const getUsers = expressAsyncHandler(async (req, res) => {
   res.json(users);
 });
 
-export { getUsers };
+export { authUser, getUsers };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import { getUsers } from '../controllers/userController.js';
+import { authUser, getUsers } from '../controllers/userController.js';
 
 router.route('/').get(getUsers);
+router.route('/login').post(authUser);
 
 export default router;
