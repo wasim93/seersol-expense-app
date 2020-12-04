94bdb85777567e27b918d6740430a727adaa74fa
Wasim Akram <muhammad.wasim93@gmail.com>
2020-12-04 12:38:48 +0500

    Expense Table Design & Fetched Data From Sample Expenses.

--- frontend/src/components/Expense.js
+++ frontend/src/components/Expense.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import { Table } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Table, Row, Col, Form, Button } from 'react-bootstrap';
+import expenses from '../expenses';
 
 const Expense = () => {
+  const [expense, setExpense] = useState(expenses);
   return (
     <div>
-      <Table striped bordered hover>
+      <Form>
+        <Row>
+          <Col md={4}>
+            <Form.Group controlId='description'>
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                type='description'
+                placeholder='Enter Description'
+              />
+            </Form.Group>
+          </Col>
+          <Col md={4}>
+            <Form.Group controlId='amount'>
+              <Form.Label>Amount</Form.Label>
+              <Form.Control type='amount' placeholder='Enter Amount' />
+            </Form.Group>
+          </Col>
+          <Col md={4}>
+            <Form.Group controlId='paidBy'>
+              <Form.Label>Paid By</Form.Label>
+              <Form.Control type='paidBy' placeholder='Paid By' />
+            </Form.Group>
+          </Col>
+          <Col className='justify-content-center'>
+            <Button variant='primary' type='submit' size='md'>
+              Submit
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+      <hr />
+      <Table striped bordered hover size='sm'>
         <thead>
           <tr>
-            <th>Description</th>
-            <th>Last Name</th>
-            <th>Username</th>
+            <th>
+              <strong>Description</strong>
+            </th>
+            <th>Amount</th>
+            <th>Paid By</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
+          {expense.map((expense) => (
+            <tr>
+              <td>{expense.title}</td>
+              <td>{expense.amount}</td>
+              <td>{expense.paidBy}</td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
