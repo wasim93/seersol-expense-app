bfcb5a2d55d109b70b8ff486eba2758e25cfd35a
Wasim Akram <muhammad.wasim93@gmail.com>
2021-01-21 17:05:06 +0500

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,17 @@ if (process.env.NODE_ENV === 'DEVELOPMENT') {
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use('/api/expenses', expenseRoutes);
 app.use('/api/users', userRoutes);
