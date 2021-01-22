7b2fae2212c61564c1b6d61d5a008598b3fedd8b
Wasim Akram <muhammad.wasim93@gmail.com>
2021-01-22 16:54:20 +0500

    Deploying to Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import connectDB from './config/db.js';
 import chalk from 'chalk';
 import morgan from 'morgan';
@@ -12,6 +13,8 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === 'DEVELOPMENT') {
   app.use(
@@ -31,12 +34,20 @@ if (process.env.NODE_ENV === 'DEVELOPMENT') {
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 app.use('/api/expenses', expenseRoutes);
 app.use('/api/users', userRoutes);
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound);
 
